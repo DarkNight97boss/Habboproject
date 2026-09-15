020ec73ac67b9ce0d80781488908cb97fd87b40b
DarkNight97boss <132572493+DarkNight97boss@users.noreply.github.com>
2026-09-15 21:59:16 +0200

    Delete CMS directory (#206)

--- CMS/app/tpl/skins/ZabboME/assets/index/js/index.js
+++
@@ -1,6 +0,0 @@
-function changeAvatar(value, target) {  
-	target = $(target);
-	if(value.length != 0){
-		target.css("background-image", "url(/username/" + value + "&action=wav&direction=2&head_direction=3&gesture=sml");
-	}
-}
--- CMS/app/tpl/skins/ZabboME/assets/js/cmsloader.js
+++
@@ -1,8 +0,0 @@
-
-/* Preloader */
-
-$(window).on('load', function() {
-	$('#status').fadeOut();
-	$('#preloader').fadeOut();
-	$('body').css({'overflow':'visible'});
-})
